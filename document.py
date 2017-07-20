6bc587878f6fb401f7a36215ea5456fe681739df
Daniel Saunders <dsaunders410@gmail.com>
2017-07-20 11:04:31 -0600

    Make various improvements

--- document.py
+++ document.py
@@ -9,7 +9,7 @@ import sys
 import shutil
 
 from lxml import etree
-from PIL import ImageFont, ImageDraw, Image
+from PIL import ImageFont, ImageDraw, Image, ImageOps
 
 class Document:
     def __init__(self, stain_level=1, noise_level=1, seed=None):
@@ -62,20 +62,36 @@ class Document:
 
     def gather_data_sources(self):
         self.transformed_words_dest_path = "data/transformed_words/"
+        self.word_image_folder_list = []
 
         #Read a file to load word images
-        self.word_image_location_file = open("paths/word_image_folder_paths.txt","r")
-        self.word_image_folder_list = self.word_image_location_file.readlines()
+        # self.word_image_location_file = open("paths/word_image_folder_paths.txt","r")
+        # self.word_image_folder_list = self.word_image_location_file.readlines()
+        for dir in os.listdir("/data/synthetic/handwriting/iamdb"):
+            files = os.listdir("/data/synthetic/handwriting/iamdb/" + dir)
 
-        for idx, item in enumerate(self.word_image_folder_list):
-            self.word_image_folder_list[idx] = item.rstrip('\r\n')
+            new_path = "/data/synthetic/handwriting/iamdb/" + dir + "/"
+
+            for idx, item in enumerate(files):
+                # subfolders = os.listdir(new_path + item)
+
+                # for idx, sub_folder_item in enumerate(subfolders):
+                files[idx] = new_path + item + "/"
+
+            self.word_image_folder_list += files
+
+
+        # for idx, item in enumerate(self.word_image_folder_list):
+            # self.word_image_folder_list[idx] = item.rstrip('\r\n')
 
         #Read a file to load background images
         self.bg_image_location_file = open("paths/word_bg_folder_paths.txt","r")
         self.bg_image_folder_list = self.bg_image_location_file.readlines()
 
-        for idx, item in enumerate(self.bg_image_folder_list):
-            self.bg_image_folder_list[idx] = item.rstrip('\r\n')
+        self.bg_image_folder_list = [ "/data/synthetic/backgrounds/" ]
+
+        # for idx, item in enumerate(self.bg_image_folder_list):
+            # self.bg_image_folder_list[idx] = item.rstrip('\r\n')
 
         #Read a file to load stain paths
         self.stain_paths_file = open("paths/stain_folder_paths.txt","r")
@@ -112,24 +128,27 @@ class Document:
         # Generate XML for DivaDID and then degrade background image
         print("- Generating degraded image - pass 1")
         first_xml, first_out = self.generate_degradation_xml(bg_full_path, 1, True, base_working_dir)
-        # subprocess.check_call(["java", "-jar", "DivaDid.jar", first_xml], stdout=subprocess.DEVNULL)
+        subprocess.check_call(["java", "-jar", "DivaDid.jar", first_xml], stdout=subprocess.DEVNULL)
 
         # Add text to degraded background image
         print("- Adding text to image")
         img = Image.open(first_out)
         text_augmented_img = self.add_text(img)
-        text_augmented_img.save("/dev/shm/sadfasdfasdf.png")
+        text_augmented_img.save("/dev/shm/" + str(self.random_seed) + "_augmented.png")
 
         # Generate XML for second pass of DivaDID. Degrade image with text
         print("- Generating degraded image - pass 2")
-        second_xml, second_out = self.generate_degradation_xml("/dev/shm/sadfasdfasdf.png", 2, True, base_working_dir)
-        # subprocess.check_call(["java", "-jar", "DivaDid.jar", second_xml], stdout=subprocess.DEVNULL)
+        second_xml, second_out = self.generate_degradation_xml("/dev/shm/" + str(self.random_seed) + "_augmented.png", 2, True, base_working_dir)
+        subprocess.check_call(["java", "-jar", "DivaDid.jar", second_xml], stdout=subprocess.DEVNULL)
 
-        # self.result = second_out
-        self.result = "/dev/shm/sadfasdfasdf.png"
+        self.result = second_out
+
+        os.remove(first_xml)
+        os.remove(second_xml)
+        os.remove(first_out)
 
 
-    def save(self, file):
+    def save(self, base_dir=None, file=None):
         """
         Save the generated document to the passed location.
 
@@ -143,8 +162,39 @@ class Document:
             print("Trying to save document before it has been generated.", file=sys.stderr)
             return
 
+        if file is None:
+            file = "img_{}.png".format(self.random_seed)
+
+        if isinstance(base_dir, str):
+            file = base_dir + '/' + file
+
         print("File saved to {}".format(file))
         shutil.copy2(self.result, file)
+
+        os.remove(self.result)
+
+    def save_ground_truth(self, base_dir=None, file=None):
+        """
+        Save the generated document to the passed location.
+
+        Note that due to the use of DivaDID, for performance reasons,
+        intermediate stages of the document generation process are saved at
+        /dev/shm. After everything is finished, the resulting product will
+        likely need to be moved from that location to a final folder.
+        """
+
+        if self.result is None:
+            print("Trying to save document before it has been generated.", file=sys.stderr)
+            return
+
+        if file is None:
+            file = "img_{}_gt.png".format(self.random_seed)
+
+        if isinstance(base_dir, str):
+            file = base_dir + '/' + file
+
+        print("File saved to {}".format(file))
+        shutil.copy2(self.result_ground_truth, file)
 
 
     def white_to_alpha(self, img, color=[0, 0, 0]):
@@ -152,6 +202,9 @@ class Document:
         img[:,:,3] = 255 - img[:,:,0]
 
         img[:,:,0:3] = color
+
+        img_clipped = np.minimum(255 - img[:,:,3], 40)
+        np.putmask(img[:,:,3], img[:,:,3] > 30, img[:,:,3] + img_clipped)
 
 
     def add_text(self, img):
@@ -162,19 +215,26 @@ class Document:
 
         not_done = True
         height_scale = -1
+        color = np.array((53, 52, 46))
 
         background_width = img.size[0]
         background_height = img.size[1]
 
-        ground_truth = Image.new("l", (background_width, background_height), 1)
+        ground_truth = Image.new("1", (background_width, background_height), 1)
+        print(np.random.normal(0, 0.2, 2))
 
         num_lines = np.rint(np.clip(np.random.normal(15, 4), 3, 30))
         text_start_position = np.clip(np.random.normal(0, 0.2, 2), 0.01, 1)
         text_end_position = np.clip(np.random.normal(1, 0.2, 2), 0, 1)
         space_between_words = int(np.clip(np.random.normal(1, 0.1, 1), 0, 0.05) * background_width)
 
+        line_height_variation = np.clip(np.random.normal(1, 0.1, 1), -.5, .5)
+        line_start_variation = np.clip(np.random.normal(1, 0.1, 1), 0, .5)
+
         avg_line_vertical_scale = (text_end_position[0] - text_start_position[0]) / num_lines
         avg_line_height = background_height * avg_line_vertical_scale
+
+        # TODO Make sure text does not get scaled too extrememely (50%-150% ?)
 
         img = img.convert("RGBA")
 
@@ -190,12 +250,7 @@ class Document:
         x_offset = int(np.rint(text_start_position[0] * background_width))
         y_offset = int(np.rint(text_start_position[1] * background_height))
 
-        print("DEBUG: text_start_position {}".format(text_start_position))
-        print("DEBUG: x_offset {} y_offset {}".format(x_offset, y_offset))
-
-        # while(not_done):
-        for counter in range(7):
-            print(x_offset, y_offset)
+        while(not_done):
 
             word_image_name = random.choice(os.listdir(word_rand_folder))
             word_full_path = word_rand_folder + word_image_name
@@ -205,23 +260,35 @@ class Document:
             new_word_width = int(np.rint(word.size[0] * avg_word_scale_factor))
             new_word_height = int(np.rint(word.size[1] * avg_word_scale_factor))
 
-            if x_offset + new_word_width > int(np.rint(text_end_position[1] * background_height)):
+            if x_offset + new_word_width > int(np.rint(text_end_position[1] * background_width)):
+                if y_offset + new_word_height > int(np.rint(text_end_position[0] * background_height)):
+                    break
+
                 x_offset = int(np.rint(text_start_position[0] * background_width))
-                y_offset += avg_line_height
+                y_offset += int(avg_line_height + (avg_line_height * line_height_variation))
 
             word = word.resize((new_word_width, new_word_height), Image.BICUBIC)
 
             word = np.asarray(word).copy()
+            ground_truth_word = np.asarray(word).copy()
 
-            self.white_to_alpha(word)
+            color += np.random.randint(-2, 3, size=3)
+            self.white_to_alpha(word, color=color)
+            self.white_to_alpha(ground_truth_word)
 
             word = Image.fromarray(word)
+            ground_truth_word = Image.fromarray(ground_truth_word)
 
             word = word.crop((-x_offset, -y_offset, background_width - x_offset, background_height - y_offset))
+            ground_truth_word = ground_truth_word.crop((-x_offset, -y_offset, background_width - x_offset, background_height - y_offset))
 
             img = Image.alpha_composite(img, word)
+            ground_truth.paste(ground_truth_word, mask=ground_truth_word)# = Image.alpha_composite(ground_truth, ground_truth_word)
 
             x_offset += new_word_width + space_between_words
+
+        self.result_ground_truth = "/dev/shm/" + str(self.random_seed) + "_gt.png"
+        ground_truth.save(self.result_ground_truth)
 
         return img
 
@@ -242,12 +309,12 @@ class Document:
         output_file_name = "degraded_{}_{}.png".format(self.random_seed, index)
         xml_file_name = "degradation_script_{}_{}.xml".format(self.random_seed, index)
 
-        output_full_path = "data/output/" + output_file_name
-
         if save_location is None:
             xml_full_path = "data/xml/" + xml_file_name
+            output_full_path = "data/output/" + output_file_name
         else:
             xml_full_path = save_location + xml_file_name
+            output_full_path = save_location + output_file_name
 
         root = etree.Element("root")
 
--- script_generator.py
+++ script_generator.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 import argparse
 import sys
+import traceback
 
 if sys.version_info < (3, 0):
     sys.stdout.write("Python 2 is not supported. Please use Python 3\n")
     sys.exit(1)
 
 from document import Document
+from multiprocessing import Pool
 
 def check_output_count(value):
     value = int(value)
@@ -24,6 +26,21 @@ def check_level(value):
 
     print("Usage: python script_generator.py output_num stain_level(1-5) text_noisy_level(1-5)")
 
+def generate_single_image(iter):
+    print("Generating image #{}".format(iter + 1))
+
+    # 99585
+    try:
+        document = Document(args.stain_level, args.text_noise_level)
+
+        document.create()
+        document.save(base_dir="/data/synthetic_trial_results/")
+        document.save_ground_truth(base_dir="/data/synthetic_trial_results/")
+
+    except Exception as e:
+        print(document.random_seed)
+        traceback.print_tb(e.__traceback__)
+
 parser = argparse.ArgumentParser(description='Generate some images.')
 parser.add_argument('output_count', metavar='N', type=check_output_count, nargs='?', default=5,
                     help='number of images to generate')
@@ -31,18 +48,15 @@ parser.add_argument('stain_level', metavar='S', type=check_level, nargs='?', def
                     help='amount of noise in stains')
 parser.add_argument('text_noise_level', metavar='T', type=check_level, nargs='?', default=1,
                     help='amount of noise in text')
+parser.add_argument('--output_dir', metavar='DIR', default=None,
+                    help='directory where final images are to be saved')
 
 args = parser.parse_args()
 
 print("Generating {} images".format(args.output_count))
 
-for image_count in range(args.output_count):
+pool = Pool()
 
-    print("Generating image #{}".format(image_count + 1))
+pool.map(generate_single_image, range(args.output_count))
 
-    document = Document(args.stain_level, args.text_noise_level, seed=1234)
-
-    document.create()
-    document.save("text.png")
-    exit()
 
