15b29fc3d704db244663689796082b3396811f74
Daniel Saunders <dsaunders410@gmail.com>
2017-09-29 15:10:10 -0600

    Modify simple crop documents to overlap patches deterministically

--- crop_documents_simple.py
+++ crop_documents_simple.py
@@ -10,53 +10,94 @@ import cv2
 import numpy
 
 from multiprocessing import Pool
+from termcolor import colored
 
-SOURCE="/home/saund9/git/CycleGAN/data_d/first_stage/"
-DEST="/home/saund9/git/CycleGAN/data_d/trainB/"
+SOURCE="/data/input_for_trainB/"
+DEST="/data/trainB/"
 NUM_SAMPLES_PERIMAGE=10
 
 def insert_value(orig, value):
     return orig[:-4] + "_" + str(value) + orig[-4:]
 
 def verify_file(file):
+    file = DEST + file
     try:
         image = cv2.imread(file)
 
-        if image.shape != (256, 256):
-            raise AssertionError("Image {} was invalid".format(file))
+        if image.shape[0] != 256 and image.shape[1] != 256:
+            raise AssertionError("Image {} was invalid. Shape: {}".format(file, image.shape))
 
-    except Exception:
+    except Exception as e:
+        print(colored(e, 'red'))
         print("Removing {}".format(file))
-        shutil.remove(file)
+        os.remove(file)
+
+def update_locations(distance, first, second):
+    if distance > 256:
+        first += 128
+        second += 128
+    else:
+        first += distance
+        second += distance
+
+    return first, second
 
 def convert(file):
     file = SOURCE + file
     print(file)
-    base_original = cv2.imread(file)
+    original = cv2.imread(file)
+    base_original = original.copy()
 
     if base_original.shape[0] < 256 or base_original.shape[1] < 256:
+        print(colored("Image is too small", 'red'))
         return
 
-    print("Croppping {} {}".format(file, base_original.shape))
+    top_left_x = 0
+    top_left_y = 0
+    bottom_right_x = 256
+    bottom_right_y = 256
+    distance_from_edge = base_original.shape[1] - bottom_right_x
+    distance_from_bottom = base_original.shape[0] - bottom_right_y
 
-    for iter in range(NUM_SAMPLES_PERIMAGE):
-        original = base_original.copy()
+    x_block = 0
+    y_block = 0
 
-        top_left_y = random.randint(0, original.shape[0] - 256)
-        top_left_x = random.randint(0, original.shape[1] - 256)
-        bottom_right_x = top_left_x + 256
-        bottom_right_y = top_left_y + 256
+    while distance_from_bottom != 0:
+        while distance_from_edge != 0:
+            base_original = original.copy()
+            cropped_partition = base_original[top_left_y:bottom_right_y, top_left_x:bottom_right_x]
 
-        original = original[top_left_y:bottom_right_y, top_left_y:bottom_right_y]
+            new_file = DEST + os.path.basename(file)
+            new_file = insert_value(new_file, "{}_{}".format(y_block, x_block))
 
-        file = DEST + os.path.basename(file)
-        file = insert_value(file, iter)
+            cv2.imwrite(new_file, cropped_partition)
+            # print("tl: ({}, {}) br: ({}, {}) distance: {}".format(top_left_x, top_left_y, bottom_right_x, bottom_right_y, distance_from_edge))
 
-        cv2.imwrite(file, original)
+            x_block += 1
+
+            distance_from_edge = base_original.shape[1] - bottom_right_x
+
+            top_left_x, bottom_right_x = update_locations(distance_from_edge,
+                                                          top_left_x,
+                                                          bottom_right_x)
+
+
+        y_block += 1
+        x_block = 0
+        top_left_x = 0
+        bottom_right_x = 256
+
+        distance_from_bottom = base_original.shape[0] - bottom_right_y
+        distance_from_edge = base_original.shape[1] - bottom_right_x
+
+        top_left_y, bottom_right_y = update_locations(distance_from_bottom,
+                                                      top_left_y,
+                                                      bottom_right_y)
+
 
 
 pool = Pool()
 
 pool.map(convert, os.listdir(SOURCE))
 
-pool.map(verify, os.listdir(DEST))
+# pool.map(verify_file, os.listdir(DEST))
